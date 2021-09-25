69fd8c46b460eff22d743ae3c98f18c05a5a1c21
Nerdenator <a.mcruer@live.com>
2021-09-24 23:07:41 -0500

    start on instructions for next commit.

--- fig12_03.c
+++ fig12_03.c
@@ -20,3 +20,56 @@ void printList( ListNodePtr currentPtr );
 void instructions ( void );
 
 int main( void )
+{
+    ListNodePtr startPtr = NULL; /* initially there are no nodes */
+    int choice; /* user's choice */
+    char item;  /* char entered by user */
+
+    instructions(); /* display the menu */
+    printf("? ");
+    scanf("%d", &choice);
+
+    /* loop while user does not choose 3 */
+    while ( choice != 3 ) {
+        switch (choice) 
+        {
+            case 1:
+                printf( "Enter a character: " );
+                scanf( "\n%c", &item );
+                insert( &startPtr, item );  /* insert item into list */
+                printList( startPtr );
+                break;
+
+            case 2: /* delete an element */
+                /* if the list is not empty */
+                if ( !isEmpty( startPtr ) ) {
+                    printf( "Enter character to be deleted: " );
+                    scanf( "\n%c", &item );
+                    
+                    /* if character is found, remove it */
+                    if ( delete (&startPtr, item) ) {   /* remove item */
+                        printf( "%c deleted.\n", item );
+                        printList( startPtr );
+                    }   /* end if */
+                    else {
+                        printf( "%c not found.\n\n", item);
+                    }   /* end else */
+                }   /* end if */
+                else {
+                    printf( "List is empty.\n\n" );
+                }   /* end else */
+                break;
+
+            default:
+                printf( "Invalid choice.\n\n" );
+                instructions();
+                break;
+        }   /* end switch */
+
+        printf( "? " );
+        scanf( "%d", &choice );
+    }   /* end while */
+
+    printf( "End of run.\n" );
+    return 0;   /* indicates successful termination */
+}   /* end main */
